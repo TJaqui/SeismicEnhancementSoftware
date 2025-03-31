a79550fd36b324a16ef88fbc44052eac3eca84c2
TJaqui <torresjaqui32@gmail.com>
2025-03-31 11:24:27 -0500

    feat: enhance button funtionality

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
 import sys
+import segyio
+import utils
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QVBoxLayout
 from PyQt5.uic import loadUi
 from matplotlib.figure import Figure
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QVBoxLayout
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-import segyio
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None):
         fig = Figure()
@@ -16,13 +21,16 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         loadUi('seismicaplication.ui', self)  # Pasamos self para enlazar los elementos de la UI
         self.data = None
+        self.dataEnhanced = None
         self.canvas = MplCanvas(self)
+        #self.canvas1 = MplCanvas(self)
 
         # Add canvas to a layout inside the main window
-        self.layout = QVBoxLayout(self.centralwidget)  # Ensure `centralwidget` is in the UI
+        self.layout = QVBoxLayout(self.centralwidget)  
         #self.layout.addWidget(self.canvas)
         self.canvas.setFixedSize(500, 300)
-        self.openfile.clicked.connect(self.browsefiles)  # Ahora sí debería reconocer el botón
+        self.openfile.clicked.connect(self.browsefiles)  
+        self.enhancedata.clicked.connect(self.enhanceData)
 
     def browsefiles(self):
         fname, _ = QFileDialog.getOpenFileName(self, 'Open file', 'C:/')
@@ -37,7 +45,22 @@ class MainWindow(QMainWindow):
         x = [0, 1, 2, 3, 4, 5]
         y = [0, 1, 4, 9, 16, 25]
         self.canvas.ax.imshow(self.data, cmap="gray")
-        self.canvas.ax.legend()
+        #self.canvas.ax.legend()
+        self.canvas.draw()
+    
+    def enhanceData(self):
+        self.layout.addWidget(self.canvas, stretch=1)
+        self.canvas.ax.clear()
+        self.dataEnhanced = self.data
+        TestData, top, bot, lf, rt = utils.padding(self.dataEnhanced)
+        TestData -=TestData.min()
+        TestData /=TestData.max()
+        
+        patches = utils.patchDivision(TestData)
+        self.dataEnhanced = utils.seismicEnhancement(patches,TestData.shape)
+        self.dataEnhanced = self.dataEnhanced[top:self.dataEnhanced.shape[0]-bot,lf:self.dataEnhanced.shape[1]-rt]
+        self.canvas.ax.imshow(self.dataEnhanced, cmap="gray")
+        #self.canvas.ax.legend()
         self.canvas.draw()
 
 app = QApplication(sys.argv)
---
+++ utils.py
@@ -0,0 +1,97 @@
+import math
+import torch
+import numpy as np
+from sporco import array,plot,util
+from models.Attention_unet import AttU_Net
+from tqdm import tqdm
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def mirror_padding(image, top_padding, bottom_padding, left_padding, right_padding):
+    h, w = image.shape
+    new_h = h + top_padding + bottom_padding
+    new_w = w + left_padding + right_padding
+    padded_image = np.zeros((new_h, new_w), dtype=image.dtype)
+    
+    padded_image[top_padding:top_padding + h, left_padding:left_padding + w] = image
+    
+    if top_padding > 0:
+        padded_image[:top_padding, left_padding:left_padding + w] = image[:top_padding][::-1]
+    
+    if bottom_padding > 0:
+        padded_image[top_padding + h:, left_padding:left_padding + w] = image[-bottom_padding:][::-1]
+    
+    if left_padding > 0:
+        padded_image[top_padding:top_padding + h, :left_padding] = image[:, :left_padding][:, ::-1]
+    
+    if right_padding > 0:
+        padded_image[top_padding:top_padding + h, left_padding + w:] = image[:, -right_padding:][:, ::-1]
+    
+    if top_padding > 0 and left_padding > 0:
+        padded_image[:top_padding, :left_padding] = image[:top_padding, :left_padding][::-1, ::-1]
+    
+    if top_padding > 0 and right_padding > 0:
+        padded_image[:top_padding, left_padding + w:] = image[:top_padding, -right_padding:][::-1, ::-1]
+    
+    if bottom_padding > 0 and left_padding > 0:
+        padded_image[top_padding + h:, :left_padding] = image[-bottom_padding:, :left_padding][::-1, ::-1]
+    
+    if bottom_padding > 0 and right_padding > 0:
+        padded_image[top_padding + h:, left_padding + w:] = image[-bottom_padding:, -right_padding:][::-1, ::-1]
+    
+    return padded_image
+
+def padding(img):
+    padd_img = img
+    if (img.shape[0] % 128 != 0):
+        pad = math.ceil(img.shape[0]/128)*128 - img.shape[0]
+        if pad%2==0:
+            top = int(pad/2)
+            bot = int(pad/2)
+        else:
+            top = math.ceil(pad/2)
+            bot = math.floor(pad/2)
+        padd_img = mirror_padding(img,top,bot,0,0)
+    if (img.shape[1] % 128 != 0):
+        pad = math.ceil(img.shape[1]/128)*128 - img.shape[1]
+        if pad%2==0:
+            lf =int(pad/2)
+            rt = int(pad/2)
+        else:
+            lf = math.ceil(pad/2)
+            rt = math.floor(pad/2)
+        padd_img = mirror_padding(padd_img,0,0,lf,rt)
+    return padd_img, top, bot, lf, rt
+
+def patchDivision(data, step=16):
+    singleffid = data.copy()
+    blksz = (128,128)
+    stpsz = (step,step)
+    blcks_corrupt = array.extract_blocks(singleffid, blksz, stpsz)
+    subblcks_corrupt = blcks_corrupt.transpose(2,0,1)
+    subblcks_corrupt = torch.from_numpy(subblcks_corrupt).unsqueeze(1)
+    return subblcks_corrupt
+
+def seismicEnhancement(data,shape,step=16):
+
+    blksz = (128,128)
+    stpsz = (step,step)
+    model = AttU_Net(img_ch=1,output_ch=1).to(device)
+    model.eval()
+    model.load_state_dict(torch.load('checkpoints/att_u_fine.pt', weights_only=False))
+    data_loader = torch.utils.data.DataLoader(data, batch_size=40)
+    denoised_tensor_list = []
+
+    for batch in tqdm(data_loader):
+        with torch.no_grad():
+            denoised_batch = model(batch.to(device).float())
+            denoised_tensor_list.append(denoised_batch.cpu())
+    recov_blcks = torch.cat(denoised_tensor_list, dim=0)
+    
+    del denoised_tensor_list
+    print(recov_blcks.shape)
+    Urec  = recov_blcks.squeeze().permute(1,2,0).cpu().numpy()
+    Urec = Urec.reshape(np.prod(blksz), -1)
+    #start = timeit.default_timer()
+    imgd_median = array.combine_blocks(Urec.reshape(blksz + (-1,)), shape, stpsz, np.median)
+    #print("creating image from patching, time is:",
+    #            timeit.default_timer() - start)
+    return imgd_median
