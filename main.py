374129e42cd1a756a9b113eaf84463aad9286150
TJaqui <torresjaqui32@gmail.com>
2025-05-07 14:12:57 -0500

    feat: save data and select data with mouse

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QVBoxLayout,
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
-
+global fname, x_start, x_end, y_start, y_end, datamin, datamax
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None):
         fig = Figure()
@@ -107,6 +107,96 @@ class RangeDialog(QDialog):
         except Exception as e:
             # Ignore invalid input temporarily
             pass
+from matplotlib.widgets import RectangleSelector
+
+class RangeDialogAd(QDialog):
+    def __init__(self, parent=None, data=None):
+        super().__init__(parent)
+        self.setWindowTitle("Set Ranges")
+        self.setFixedSize(400, 400)
+
+        self.data = data
+        self.canvas = MplCanvas(self)
+
+        self.x_from = QLineEdit("0")
+        self.x_to = QLineEdit(str(data.shape[1]) if data is not None else "500")
+        self.y_from = QLineEdit("0")
+        self.y_to = QLineEdit(str(data.shape[0]) if data is not None else "500")
+
+        layout = QVBoxLayout()
+
+        x_layout = QHBoxLayout()
+        x_layout.addWidget(QLabel("x from"))
+        x_layout.addWidget(self.x_from)
+        x_layout.addWidget(QLabel("-"))
+        x_layout.addWidget(self.x_to)
+        layout.addLayout(x_layout)
+
+        y_layout = QHBoxLayout()
+        y_layout.addWidget(QLabel("y from"))
+        y_layout.addWidget(self.y_from)
+        y_layout.addWidget(QLabel("-"))
+        y_layout.addWidget(self.y_to)
+        layout.addLayout(y_layout)
+
+        layout.addWidget(self.canvas)
+
+        btn_layout = QHBoxLayout()
+        ok_btn = QPushButton("ok")
+        cancel_btn = QPushButton("cancel")
+        ok_btn.clicked.connect(self.accept)
+        cancel_btn.clicked.connect(self.reject)
+        btn_layout.addStretch()
+        btn_layout.addWidget(ok_btn)
+        btn_layout.addWidget(cancel_btn)
+        layout.addLayout(btn_layout)
+
+        self.setLayout(layout)
+
+        if self.data is not None:
+            self.canvas.ax.imshow(self.data, cmap="gray")
+            self.selector = RectangleSelector(
+                self.canvas.ax,
+                self.on_select,
+                useblit=True,
+                button=[1],  # Left click
+                minspanx=5,
+                minspany=5,
+                spancoords='pixels',
+                interactive=True
+            )
+            self.canvas.draw()
+
+    def on_select(self, eclick, erelease):
+        x1, y1 = int(eclick.xdata), int(eclick.ydata)
+        x2, y2 = int(erelease.xdata), int(erelease.ydata)
+
+        # Ordenar coordenadas
+        x_start, x_end = sorted((x1, x2))
+        y_start, y_end = sorted((y1, y2))
+
+        self.x_from.setText(str(x_start))
+        self.x_to.setText(str(x_end))
+        self.y_from.setText(str(y_start))
+        self.y_to.setText(str(y_end))
+
+        self.update_plot()
+
+    def get_ranges(self):
+        return (int(self.x_from.text()), int(self.x_to.text()),
+                int(self.y_from.text()), int(self.y_to.text()))
+
+    def update_plot(self):
+        try:
+            x_start, x_end, y_start, y_end = self.get_ranges()
+            if self.data is not None:
+                cropped = self.data[y_start:y_end, x_start:x_end]
+                self.canvas.ax.clear()
+                self.canvas.ax.imshow(cropped, cmap="gray")
+                self.canvas.draw()
+        except Exception:
+            pass
+
     
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -114,6 +204,8 @@ class MainWindow(QMainWindow):
         loadUi('seismicaplication.ui', self)  
         self.data = None
         self.dataEnhanced = None
+        self.datatoEnhanced = None
+
         self.canvas = MplCanvas(self)
         self.progressbar = QProgressBar()
         self.progressbar.setMinimum(0)
@@ -163,10 +255,11 @@ class MainWindow(QMainWindow):
         self.beforeenhancement.clicked.connect(self.showBeforeEnhancement)  
         self.adapttodata.clicked.connect(self.showRangeDialog)  
         self.afterenhancement.clicked.connect(self.showAfterEnhancement) 
-        
+        self.savefile.clicked.connect(self.saveData) 
         self.labeldata.setStyleSheet("font-weight: bold;")
 
     def browsefiles(self):
+        global fname
         fname, _ = QFileDialog.getOpenFileName(self, 'Open file', 'C:/')
         print(fname)
         try:  
@@ -182,9 +275,18 @@ class MainWindow(QMainWindow):
             pass
 
     def plotsgy(self,file):
-        
+            
+            global datamin, datamax
+
             file = segyio.open(file,ignore_geometry=True)
             self.data = file.trace.raw[:].T
+            
+            datamin =  self.data.min()
+            datamax = self.data.max()
+            
+            self.data -= self.data.min()
+            self.data /= self.data.max()
+
             self.layout.addWidget(self.canvas, stretch=1)
             self.layout.setContentsMargins(0, 30, 0, 0)
             self.canvas.lower() 
@@ -193,7 +295,7 @@ class MainWindow(QMainWindow):
             self.canvas.draw()
         
     def showRangeDialog(self):
-        dialog = RangeDialog(self, data=self.data)
+        dialog = RangeDialogAd(self, data=self.data)
         if dialog.exec_() == QDialog.Accepted:
             x_start, x_end, y_start, y_end = dialog.get_ranges()
             print(f"Selected range: x={x_start}-{x_end}, y={y_start}-{y_end}")
@@ -204,14 +306,18 @@ class MainWindow(QMainWindow):
                 self.canvas.draw()
                 
     def enhanceData(self):
+        
         try:
-            
-            
+            global x_start, x_end, y_start, y_end
+            dialog = RangeDialog(self, data=self.data)
+            if dialog.exec_() == QDialog.Accepted:
+                x_start, x_end, y_start, y_end = dialog.get_ranges()
             QApplication.processEvents()  # <- Forzar actualización UI
-            
             self.dataEnhanced = self.data
-            TestData, top, bot, lf, rt = utils.padding(self.dataEnhanced)
+            self.datatoEnhanced = self.data[y_start: y_end,x_start: x_end ].copy()
             
+            TestData, top, bot, lf, rt = utils.padding(self.datatoEnhanced)
+     
             QApplication.processEvents()
             self.layout.addWidget(self.progressbar)
             self.progressbar.setValue(0)
@@ -221,17 +327,18 @@ class MainWindow(QMainWindow):
             time.sleep(0.01)
             QApplication.processEvents()
             self.progressbar.setValue(20)
-            patches = utils.patchDivision(TestData)
+            patches = utils.patchDivision(TestData.copy())
             self.progressbar.setValue(40)
             QApplication.processEvents()
             time.sleep(0.01)
             self.progressbar.setValue(60)
-            
+     
             self.layout.addWidget(self.canvas, stretch=1)
             self.canvas.ax.clear()
             time.sleep(0.01)
             
-            self.dataEnhanced = utils.seismicEnhancement(patches, TestData.shape)
+            self.datatoEnhanced = utils.seismicEnhancement(patches, TestData.shape)
+      
             self.progressbar.setValue(70)
             time.sleep(0.01)
             self.progressbar.setValue(80)
@@ -241,9 +348,9 @@ class MainWindow(QMainWindow):
             self.progressbar.deleteLater()
             QApplication.processEvents()
             time.sleep(0.3)
-            
-            
-            self.dataEnhanced = self.dataEnhanced[top:self.dataEnhanced.shape[0]-bot, lf:self.dataEnhanced.shape[1]-rt]
+ 
+            self.dataEnhanced[ y_start: y_end, x_start: x_end ] = self.datatoEnhanced[top:self.datatoEnhanced.shape[0]-bot, lf:self.datatoEnhanced.shape[1]-rt]
+       
             self.canvas.ax.imshow(self.dataEnhanced, cmap="gray")
             self.canvas.draw()
             
@@ -275,7 +382,9 @@ class MainWindow(QMainWindow):
             self.canvas.draw() 
 
     def saveData(self):
-        utils.save2dData(self.dataEnhanced)
+        
+        dname = QFileDialog.getExistingDirectory(self, 'Open file', 'C:/')
+        utils.save2dData(self.dataEnhanced,fname, dname, datamin, datamax)
 
 
 
