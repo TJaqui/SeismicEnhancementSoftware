807db194a981677c7d4647a8a24336543b35dd33
TJaqui <torresjaqui32@gmail.com>
2025-07-16 13:16:12 -0500

    docs: about dialog updated

--- ui/dialogs/about_dialog.py
+++ ui/dialogs/about_dialog.py
@@ -21,13 +21,17 @@ class AboutDialog(QDialog):
         title.setStyleSheet("color: #1E1E1E;")
         layout.addWidget(title)
 
-        desc = QLabel("This work was funded by the Vicerrectoría de Investigacion y Extensi´on from Universidad Industrial de Santander under Project 3925.")
+        desc = QLabel("The software focuses on enhancing 2D post-stack seismic images using a neural network model " \
+        "to attenuate seismic noise. It also supports 3D processing by enhancing slices independently. Additionally, " \
+        "it includes a domain adaptation function, which adapts the noise characteristics from field data to clean images. " \
+        "This allows fine-tuning of the pretrained network so that it learns the new noise representation effectively.")
         desc.setWordWrap(True)
         desc.setAlignment(Qt.AlignCenter)
         desc.setStyleSheet("font-size: 13px; color: #4D4D4D;")
         layout.addWidget(desc)
 
-        than = QLabel("This work was funded by the Vicerrectoría de Investigacion y Extensión from Universidad Industrial de Santander under Project 3925.")
+        than = QLabel("This work was funded by the Vicerrectoría de Investigacion y Extensión from Universidad Industrial de Santander under Project 3925")
+        
         than.setWordWrap(True)
         than.setAlignment(Qt.AlignCenter)
         than.setStyleSheet("font-size: 13px; color: #4D4D4D;")
@@ -49,11 +53,11 @@ class AboutDialog(QDialog):
         for path,maximun_size in zip(top_logos,maximun_size):
             print(path, maximun_size)
             try:
-                pixmap = QPixmap(path).scaled(100, 100, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+                pixmap = QPixmap(path).scaled(80, 80, Qt.KeepAspectRatio, Qt.SmoothTransformation)
                 label = QLabel()
                 label.setPixmap(pixmap)
                 label.setAlignment(Qt.AlignCenter)
-                label.setFixedSize(100, 100)  # Make the label exactly the same size
+                label.setFixedSize(80, 80)  # Make the label exactly the same size
                 top_row_layout.addWidget(label)
             except Exception as e:
                 print(f"Failed to load {path}: {e}")
@@ -63,11 +67,11 @@ class AboutDialog(QDialog):
         # === Bottom row: 1 centered logo ===
         bottom_logo_path = "resources/icons/uis.png"
         try:
-            pixmap = QPixmap(bottom_logo_path).scaled(100, 100, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+            pixmap = QPixmap(bottom_logo_path).scaled(80, 80, Qt.KeepAspectRatio, Qt.SmoothTransformation)
             label = QLabel()
             label.setPixmap(pixmap)
             label.setAlignment(Qt.AlignCenter)
-            label.setFixedSize(100, 100)
+            label.setFixedSize(80, 80)
             contributors_layout.addWidget(label, alignment=Qt.AlignHCenter)
         except Exception as e:
             print(f"Failed to load {bottom_logo_path}: {e}")
